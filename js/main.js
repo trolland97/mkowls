c3ca341336f7bdc5223b6b5fa3712980ab2a6e0d
trolland97 <trolland97@gmail.com>
2023-05-18 17:40:39 +0200

    fix

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+    $('.fa-bars').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.nav').toggleClass('nav-toggle');
+    });
+
+    $(window).on('load scroll',function() {
+        $('.fa-bars').removeClass('fa-times');
+        $('.nav').removeClass('nav-toggle');
+
+        if($(window).scrollTop() >10){
+            $('header').addClass('header-active');
+        }else {
+            $('header').removeClass('header-active');
+        }
+    });
+
+    $('.gallery').magnificPopup({
+        delegate:'a',
+        type:'image',
+        gallery:{
+            enabled:true
+        }
+    });
+
+    var d = new Date();
+    document.getElementById("yearFooter").innerHTML = d.getFullYear();
+
+});
